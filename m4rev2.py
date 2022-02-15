85ac1f8bab7c2cb674499905699ac2be539a1c1c
DLyman2022 <damion.lyman@mymail.champlain.edu>
2022-02-15 16:42:08 -0500

    revised code... sorta

--- m4rev2.py
+++ m4rev2.py
@@ -16,9 +16,10 @@ def get_all_objs(content, vimtype):
         obj.update({managed_object_ref:managed_object_ref.name})
     return obj
 
-# For this one, I looked over https://github.com/vmware/pyvmomi-community-samples/blob/master/samples/vminfo_quick.py
+# This function presents the user some choices in which they can use to implement the functions. 
 
-def main(content, vcenter):
+
+def main_menu(content, vcenter):
 
     choice = 0
     while choice == 0:
@@ -26,35 +27,35 @@ def main(content, vcenter):
         print("[2] List VMs by Name")
         print("[3] Print AboutInfo")
         print("[4] Quit the program")
-        answer = input("Choose an option: ")
-        if answer == "0":
+        userChoice = input("Choose an option: ")
+        if userChoice == "0":
             print("0 is not a valid option, please choose one of the listed options")
-        elif answer == "1":
-            session = content.sessionManager.currentSession
+        elif userChoice == "1":
+            s = content.sessionManager.currentSession
             print("You have selected the Login Info option.")
             print("Information from current session: ")
-            print("Your username is: " + session.userName)
-            print("Your IP Address is: " + session.ipAddress)
+            print("Your username is: " + s.userName)
+            print("Your IP Address is: " + s.ipAddress)
             print("Your vCenter Server is: " + vcenter)
-        elif answer == "2":
+        elif userChoice == "2":
             print("You have chosen the List VMs by Name option.")
             filt = input("Please enter a filter (Leave blank to print all VMs): ")
             scan = [vm for vm in get_all_objs(content, [vim.VirtualMachine])]
             vms = host_info(scan, filt)
-            vms_parse = parsing_info(vms)
+            vms_parse = vmParse(vms)
             print(vms_parse)
 
-        elif answer == "3":
+        elif userChoice == "3":
             print("You have chosen to display the AboutInfo option.")
             print(content.about)
             print("Your content version number is: " + content.about.version)
-        elif answer == "4":
+        elif userChoice == "4":
             print("You have chosen to terminate the program.")
             print("Exiting program...")
             break
 
 # For this one, I looked over https://github.com/vmware/pyvmomi-community-samples/blob/master/samples/vminfo_quick.py
-def parsing_info(vmac):
+def vmParse(vmac):
     for virtual in vmac:
             print("---" * 5)
             print("Name of Virtual Machine: {0}".format(virtual["vm_name"]))
@@ -68,20 +69,24 @@ def parsing_info(vmac):
 # Only had to change to make the filter work correctly, as well as have it iterate through the list a bit differently.
 
 def host_info(vm, search):
-    vm_info = []
+    vm_information = []
     print("The chosen filter is: " + search)
-    for virtual_machine in vm:
-        virtual_machine_name = virtual_machine.name
-        if search in virtual_machine_name:
-            
-            summary = vars(virtual_machine.summary)
-            vm_name = virtual_machine.name
-            ip_address = summary.guest.ipAddress
-            power_state = summary.runtime.powerState
-            memory = int(summary.config.memorySizeMB / 1024)
-            cpu_count = summary.config.numCpu
-    return vm_info
-
+    
+    for virtual in vm:
+        vm_name = virtual.name
+        if search in vm_name:
+            host_summary = vars(virtual.summary)
+            host = {}
+            host["vm_name"] = virtual.name
+            host["ip_address"] = host_summary["guest"].ipAddress
+            host["power_state"] = host_summary["runtime"].powerState
+            host["memory"] = int(host_summary["config"].memorySizeMB / 1024)
+            host["cpu_count"] = host_summary["config"].numCpu
+            vm_information.append(host)
+    return vm_information
+    
+# This was adapted from our intial lines of code in class from your (logging in), the only thing that is more-or-less added was
+# opening it with a creds file (rather than hardcoding it in like we did previously). 
 def user_login():
     with open('/home/champuser/SYS350/automation/SYS350/info.json')  as credfile:
         object = json.load(credfile)
@@ -95,6 +100,6 @@ def user_login():
 
 if __name__ == "__main__":
     info = user_login()
-    Info = info[0]
+    content = info[0]
     vcenter_name = info[1]
-    main(Info, vcenter_name)
+    main_menu(content, vcenter_name)
